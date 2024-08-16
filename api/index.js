28999baa8785aee08a5e52c82bc4aa308bff75f0
dharshansidharth <dharshansidharth2004@gmail.com>
2024-08-16 21:30:43 +0530

    created database connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const mongoose = require('mongoose')
+
+try{
+mongoose.connect(process.env.MONGO)
+console.log('connected to database')
+}
+catch(err){
+    throw err
+}
 
 
 app = express()
@@ -9,3 +18,6 @@ port = process.env.port
 app.listen(port , () => {
     console.log(`server running in port ${port}`)
 })
+
+//username = real_estate
+//pwd = zecfYA9oAzx4Y50v
