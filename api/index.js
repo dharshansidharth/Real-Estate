d0fcf6fb40e08d57206656b968472451a24b625b
dharshansidharth <dharshansidharth2004@gmail.com>
2024-12-23 15:22:17 +0530

    created database

--- api/index.js
+++ api/index.js
@@ -1,11 +1,22 @@
 import express from 'express'
 import { config } from "dotenv";
+import mongoose from 'mongoose'
 
 const app = express()
 config()
 const port = process.env.PORT
+const url = process.env.MONGO_URL
+
+try{
+      mongoose.connect(url)
+      console.log('connected to mongodb!')
+}
+catch(err){
+      throw err
+}
 
 app.listen(port , () => {
-      console.log(`Server running in port ${port}`)
+      console.log(`Server running in port ${port}!`)
 })
 
+//password = cPWIKEnVr7PFVYRz
