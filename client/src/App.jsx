d41906e162cb41149cdd7f6a2e21ce8edf0f8484
dharshansidharth <dharshansidharth2004@gmail.com>
2024-12-23 15:03:34 +0530

    created routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,20 @@ import About from './pages/About'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
+import Header from './components/Header.jsx'
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path = '/' element = {<Home />} />
-      <Route path = '/about' element = {<About />} />
-      <Route path = '/sign-in' element = {<SignIn />} />
-      <Route path = '/sign-up' element = {<SignUp />} />
-      <Route path = '/profile' element = {<Profile />} />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
 
-    </Routes>
+      </Routes>
     </BrowserRouter>
   )
 }
