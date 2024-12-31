d609a464fd82ef23f7ad656ccc3850c0a43ac445
dharshansidharth <dharshansidharth2004@gmail.com>
2024-12-31 22:22:45 +0530

    completed about page and 95% of the site

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className = 'flex flex-col gap-8 mx-4 mt-8 py-20'>
+      <h1 className = 'text-slate-700 font-bold text-3xl'>About Real Estate</h1>
+
+      <p className = 'text-gray-700'>Welcome to Real Estate, your trusted partner in finding the perfect home or selling your property with ease. We specialize in connecting people with spaces that fit their lifestyles, dreams, and needs. With a commitment to transparency and exceptional service, we are dedicated to making your real estate journey simple, rewarding, and enjoyable.</p>
+
+      <p className = 'text-gray-700'>Whether you're searching for your first home, upgrading to a larger space, or looking to sell for the best value, we offer personalized solutions tailored to your goals. Our comprehensive listings include a wide range of properties to suit every preference and budget. Using cutting-edge technology and market insights, we ensure you have all the tools needed to make confident decisions.</p>
+
+      <p className = 'text-gray-700'>At Real Estate, we believe a home is more than just a place – it’s where life happens. Our experienced team is here to guide you through every step of the process, from initial consultations to closing deals, with professionalism and care. Let us help you find not just a house, but a place you’ll love to call home.</p>
+    </div>
   )
 }
 
